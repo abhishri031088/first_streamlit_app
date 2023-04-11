200fe0659a7352d10924127d243b31078abc162c
abhishri031088 <130445284+abhishri031088@users.noreply.github.com>
2023-04-11 15:49:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,8 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
 # Display only selected Fruits or all fuits if nothing selected
-if not fruits_selected :
-  fruits_to_show = my_fruit_list
-else :
-  fruits_to_show = my_fruit_list.loc[fruits_selected]
-streamlit.dataframe(my_fruit_list)
+#if not fruits_selected :
+#fruits_to_show = my_fruit_list
+#else :
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
